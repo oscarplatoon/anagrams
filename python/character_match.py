bbe83889bd8d71865b43e8871dfa98bf4c90e360
Won Yang <won@gatech.edu>
2021-05-21 13:45:58 -0500

    adds python solution

--- python/character_match.py
+++ python/character_match.py
@@ -1,5 +1,15 @@
 # Don't forget to run your tests!
 
 def is_character_match(string1, string2):
-	pass
-	# Your code here
+	string1_list = list(string1)
+	string2_list = list(string2)
+
+	string1_list = sorted([x.lower() for x in string1_list])
+	string2_list = sorted([x.lower() for x in string2_list])
+
+	if string1_list == string2_list:
+		return True
+	else:
+		return False
+
+is_character_match('CharM', 'mARcH')
