a1be046bfbb57e1ea563741aee0dd6a943e634a6
jakewhite5 <jakewhite5@yahoo.com>
2021-05-27 20:55:46 -0700

    js portion added

--- js/characterMatch.js
+++ js/characterMatch.js
@@ -1,3 +1,10 @@
-exports.isCharacterMatch = function(string1, string2) {
 
-};
+function isCharacterMatch(string1, string2) {
+  if ((Array.from(string1.toLowerCase().replace(/\s/g, '')).sort()) == ((Array.from(string2.toLowerCase().replace(/\s/g, '')).sort()))); {
+    return true
+  }
+  return false
+}
+
+isCharacterMatch('CH Haa  TT', 'tchataht')
+
