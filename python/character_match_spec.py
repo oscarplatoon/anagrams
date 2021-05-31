50de7181a7f3805a58409e7261257d36c31f5c3c
Marc Stanley <marcson909@gmail.com>
2021-05-31 15:03:49 -0500

    completed in python by lowering, replacing whitespace & sorting then comparing. included unit tests

--- python/character_match_spec.py
+++ python/character_match_spec.py
@@ -1,13 +1,23 @@
 from character_match import is_character_match
+import unittest
 
-# This should return a bunch of trues
-print(is_character_match('charm', 'march') == True)
-print(is_character_match('zach', 'attack') == False)
-print(is_character_match('CharM', 'mARcH') == True)
-print(is_character_match('abcde2', 'c2abed') == True)
+class CharMatchTestCase(unittest.TestCase):
 
-print("This test is for the challenge quesiton")
-print(is_character_match('Anna Madrigal', 'A man and a girl') == True)
+    def test_bool(self):
+        self.assertIsInstance(is_character_match('', ''), bool)
 
+    def test_charm_march(self):
+        self.assertEqual(is_character_match('charm', 'march'), True)
 
+    def test_zach_attack(self):
+        self.assertEqual(is_character_match('zach', 'attack'), False)
+    
+    def test_charm_cases(self):
+        self.assertEqual(is_character_match('CharM', 'mARcH'), True)
+    
+    def test_abcde2_c2abed(self):
+        self.assertEqual(is_character_match('abcde2', 'c2abed'), True)
+
+    def test_spaced_strings(self):
+        self.assertEqual(is_character_match('Anna Madrigal', 'A man and a girl'), True)
 # Can you translate this driver code to unit tests?
